5762ece3c92cced38ed73fc4642d07ec8d8a67b6
Jayesh7046 <tahilianij11@gmail.com>
2023-01-17 18:05:03 +0530

    new setup init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>01</li>
+        <li>02</li>
+        <li>03</li>
+        <li>04</li>
+        <li>05</li>
+        <li>06</li>
+        <li>07</li>
+        <li>08</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
